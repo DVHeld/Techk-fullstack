6f30a8d9895473262ee0d1f0512dfe3dc0912348
Dusan Vilicic Held <dvheld@gmail.com>
2018-08-05 22:31:23 -0400

    small corrections to scraper + test views.py

--- techk/apps/scraper/scrape.py
+++ techk/apps/scraper/scrape.py
@@ -19,49 +19,25 @@ for a in a_list:
 # Extracting books
 books = []
 book_list = soup(class_='product_pod')
-html = ''
 id = 1
 
 for book in book_list:
-    # We look for the data in the individual book's page
+    # We look for the book's data in its own page
     book_url = base_url + book.find('a')['href']
     r_book = requests.get(book_url)
     soup_book = BeautifulSoup(r_book.content)
 
     book_category = soup_book.select('a[href*="category/books/"]')[0].get_text(strip=True)
-
     for category in [x for x in categories if x['name'] == book_category]:
         book_category_id = category['id']
         break
-
-    html += '</br></br> ========= book_category =========</br></br>'
-    html += str(book_category_id) + ': ' + book_category
     
-    html += '</br></br> =============================='
     book_title = soup_book.find(class_='product_main').h1.get_text(strip=True)
-    html += '</br></br> ========= book_title =========</br></br>'
-    html += book_title
-
     book_thumbnail = base_url + soup_book.find('img')['src'][6:]
-    html += '</br></br> ========= book_thumbnail =========</br></br>'
-    html += book_thumbnail + '</br></br>' + '<img src="' + book_thumbnail + '">'
-
     book_price = soup_book.find(class_='price_color').get_text(strip=True)
-    html += '</br></br> ========= book_price =========</br></br>'
-    html += book_price
-
     book_stock = True if soup_book.find(class_='instock availability').get_text(strip=True)[:8] == 'In stock' else False
-    html += '</br></br> ========= book_stock =========</br></br>'
-    html += 'true' if book_stock == True else 'false'
-
     book_description = soup_book.find(id='product_description').find_next_sibling('p').get_text(strip=True)
-    html += '</br></br> ========= book_description =========</br></br>'
-    html += book_description
-
     book_UPC = soup_book.find('td').get_text(strip=True)
-    html += '</br></br> ========= book_UPC =========</br></br>'
-    html += book_UPC
-    
 
     books.append({
         'id': id
