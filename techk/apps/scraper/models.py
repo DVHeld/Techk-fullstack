6e89f21efd39cc21c730eaef941ed95c3b2d4273
Dusan Vilicic Held <dvheld@gmail.com>
2018-08-10 00:54:10 -0400

    Various corrections and cleanup
    - Corrected database and models.
    - Changed scraper to directly store data in database.
    - Various lint cleanups.
    - Scraper now deletes all data in Category and Book tables prior to scraping.
    - Models classes now have __str__ methods useful for debugging.
    - Moved models from base app to scrape app

--- techk/apps/scraper/models.py
+++ techk/apps/scraper/models.py
@@ -1,11 +1,17 @@
+'''scraper app models'''
 #-*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
 
 class Category(models.Model):
+    '''Django model for scraped categories'''
     name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return str(self.id) + ' - ' + self.name
+
 class Book(models.Model):
+    '''Django model for scraped books'''
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=512)
     thumbnail_url = models.URLField()
@@ -13,3 +19,6 @@ class Book(models.Model):
     stock = models.NullBooleanField()
     product_description = models.CharField(max_length=1024)
     upc = models.CharField(max_length=64)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.title + ' - ' + str(self.category)
--- techk/apps/scraper/scrape.py
+++ techk/apps/scraper/scrape.py
@@ -1,11 +1,12 @@
+'''Scraper function'''
 from bs4 import BeautifulSoup
 import requests
-
+from apps.scraper.models import Category, Book
 
 def scrape():
     '''Description
-    The scrape() function scrapes http://books.toscrape.com/index.html and returns a dictionary
-    with two entries, categories and books, both containing data scraped from the mentioned website.
+    The scrape() function scrapes http://books.toscrape.com/index.html and stores the retrieved data
+    to the database.
 
     Syntax
     scrape()
@@ -14,93 +15,60 @@ def scrape():
     This function takes no parameters.
 
     Return Value
-    This function returns a dictionary returns with two entries with the keys "categories" and "books",
-    each of which contains a list of dictionaries. The "categories" entry contains a list of
-    dictionaries, each containing the keys "id" and "name". The "books" entry contains a list of
-    dictionaries, each containing the keys "id", "category_id", "title", "thumbnail_url", "price", "stock",
-    "product_description", and "upc".
-    
-    Example
-    The following example shows the usage of the scrape() function:
-    
-    result = scrape()
-    print result
-    
-    When run, the above program produces a result similar to the following, depending on the contents on the aforementioned website:
-    
-    [{
-        "categories": [
-            {
-                "id": 1,
-                "name": "Travel"
-            }, {
-                "id": 2,
-                "name": "Mystery"
-            }, {
-                "id": 3,
-                "name": "Historical Fiction"
-            }
-        ],
-        "books": [
-            {
-                "id": 1;
-                "category_id": 1,
-                "title": "It's Only the Himalayas",
-                "thumbnail_url": "http://books.toscrape.com/media/cache/6d/41/6d418a73cc7d4ecfd75ca11d854041db.jpg",
-                "price": "£45.17",
-                "stock": true,
-                "product_description": "Wherever you go, whatever you do, just ...",
-                "upc": "a22124811bfa8350"
-            }
-        ]
-    }]'''
+    This function does not return anything.'''
+
+    # Clear all tables of previous data
+    categories = Category.objects.all()
+    if categories:
+        categories.delete()
+    books = Book.objects.all()
+    if books:
+        books.delete()
 
     base_url = 'http://books.toscrape.com/'
-    r = requests.get(base_url + 'index.html')
-    soup = BeautifulSoup(r.content)
+    req = requests.get(base_url + 'index.html')
+    soup = BeautifulSoup(req.content, 'html.parser')
 
     categories = []
-    a_list = soup.select('a[href*="catalogue/category/"]')
-    id = 1
+    anchor_list = soup.select('a[href*="catalogue/category/"]')
 
-    for a in a_list:
-        a_href_category = a.get_text(strip=True)
+    for anchor in anchor_list:
+        a_href_category = anchor.get_text(strip=True)
         if a_href_category not in categories:
-            categories.append({'id': id, 'name': a_href_category})
-            id += 1
+            categories.append(Category(name=a_href_category))
+
+    Category.objects.bulk_create(categories)
+    categories = Category.objects.all()
 
     books = []
     book_list = soup(class_='product_pod')
-    id = 1
 
     for book in book_list:
         book_url = base_url + book.find('a')['href']
         r_book = requests.get(book_url)
-        soup_book = BeautifulSoup(r_book.content)
-
+        soup_book = BeautifulSoup(r_book.content, 'html.parser')
         book_category = soup_book.select('a[href*="category/books/"]')[0].get_text(strip=True)
 
-        for category in [x for x in categories if x['name'] == book_category]:
-            book_category_id = category['id']
-            break
-        
+        for category in [cat for cat in categories if cat.name == book_category]:
+            book_category_id = category.id
+
         book_title = soup_book.find(class_='product_main').h1.get_text(strip=True)
         book_thumbnail = base_url + soup_book.find('img')['src'][6:]
         book_price = soup_book.find(class_='price_color').get_text(strip=True)
-        book_stock = True if soup_book.find(class_='instock availability').get_text(strip=True)[:8] == 'In stock' else False
-        book_description = soup_book.find(id='product_description').find_next_sibling('p').get_text(strip=True)
-        book_UPC = soup_book.find('td').get_text(strip=True)
+        availability = soup_book.find(class_='instock availability').get_text(strip=True)[:8]
+        book_stock = True if availability == 'In stock' else False
+        soup_description = soup_book.find(id='product_description').find_next_sibling('p')
+        book_description = soup_description.get_text(strip=True)
+        book_upc = soup_book.find('td').get_text(strip=True)
 
-        books.append({
-            'id': id
-            , 'category_id': book_category_id
-            , 'title': book_title
-            , 'thumbnail_url': book_thumbnail
-            , 'price': book_price
-            , 'stock': book_stock
-            , 'product_description': book_description
-            , 'upc': book_UPC
-        })
-        id += 1
+        books.append(Book(
+            category_id=book_category_id,
+            title=book_title,
+            thumbnail_url=book_thumbnail,
+            price=book_price,
+            stock=book_stock,
+            product_description=book_description,
+            upc=book_upc
+        ))
 
-    return {'categories': categories, 'books': books}
+    Book.objects.bulk_create(books)
